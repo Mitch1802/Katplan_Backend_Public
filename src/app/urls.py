f210d177a9940ac32da1e88bba80b78403c47309
Mitch1802 <77403827+Mitch1802@users.noreply.github.com>
2023-07-05 13:59:43 +0200

    Update urls.py

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from core.admin import core_site
+# from core.admin import core_site
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('coreadmin/', core_site.urls),
+    path('admin/', admin.site.urls),
+    # path('coreadmin/', core_site.urls),
     path('api/user/', include('user.urls')),
     path('api/', include('katplan.urls')),
 ]
