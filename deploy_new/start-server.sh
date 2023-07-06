aa9b07c356c07b80b4ced89db49a32b4710390b6
Mitch1802 <77403827+Mitch1802@users.noreply.github.com>
2023-07-06 10:22:05 +0200

    Update start-server.sh

--- deploy_new/start-server.sh
+++ deploy_new/start-server.sh
@@ -1,1 +1,7 @@
-
+#!/usr/bin/env bash
+# start-server.sh
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+    (cd katplan; python manage.py createsuperuser --no-input)
+fi
+(cd katplan; gunicorn katplan.wsgi --user www-data --bind 0.0.0.0:80 --workers 3) &
+nginx -g "daemon off;"
